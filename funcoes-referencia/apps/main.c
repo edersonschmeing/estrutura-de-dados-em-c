a6973364ab408a5cf109267a8398622c406ab4d3
edersonschmeing <edersonschmeing@gmail.com>
2023-02-21 10:39:56 -0300

    funções passagem de parâmetro por referência

---
+++ funcoes-referencia/apps/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+//Exemplo de funções com passagem de parâmetros por valor e por referência
+
+//void troca();
+
+//void troca();
+
+void troca(int *x, int *y) {
+     int w;
+    
+    printf("### DENTRO DA FUNÇÃO ### \n");
+    printf("&x = %p | x = %p | *x = %d\n", &x, x, *x);
+    printf("&y = %p | y = %p | *y = %d\n", &y, y, *y);
+    printf("&w = %p | z = %d\n\n", &w, w); 
+
+    w = *x;
+    printf("&w = %p, z = %d\n\n", &w, w); 
+
+    *x = *y;
+    *y = w;              
+}   
+
+int main() {
+    
+   int a = 20;
+   int b = 30;
+
+   printf("### ANTES DE CHAMAR A FUNÇÃO ### \n");
+   printf("&a = %p | a = %d\n", &a, a);
+   printf("&b = %p | b = %d\n\n", &b, b);
+
+   troca(&a, &b);
+
+   printf("### DEPOIS DE CHAMAR A FUNÇÃO ### \n");
+   printf("&a = %p | a = %d\n", &a, a);
+   printf("&b = %p | b = %d\n\n", &b, b);
+    
+   return 0;
+    
+}
