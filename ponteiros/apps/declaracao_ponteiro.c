6a2df1c02788cdae6126cabec3eb73f550265112
edersonschmeing <edersonschmeing@gmail.com>
2023-02-24 20:15:36 -0300

    ponteiros

--- ponteiros/apps/declaracao_ponteiro.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a = 30;
-    int *p1 = (int*)0x7ffddb292b2c;
-    int *p = &a;
-    printf("&p = %p || p = %p || *p = %d  \n", &p, p, *p);
-    return 0;
-}
