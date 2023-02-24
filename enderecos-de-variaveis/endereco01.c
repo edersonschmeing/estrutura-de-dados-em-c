89c80a612bb8cfa93614287b75eab27602fc22a4
edersonschmeing <edersonschmeing@gmail.com>
2023-02-24 20:53:42 -0300

    enderços de váriaveis

---
+++ enderecos-de-variaveis/endereco01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+         
+    int a = 30; 
+    printf("&a = %p \n", &a);
+  
+    return 0;
+}
---
+++ enderecos-de-variaveis/endereco02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    char a = '0';
+    short b = 1;
+    int c = 3;
+    long d = 4 ; 
+    long e = 5;
+
+     // & obtem o endereço de memória das váriaveis.
+     // hexadecimal | decimal | conteúdo
+    printf("&a = %p | &a = %ld | a = %c\n", &a, &a, a);
+    printf("&b = %p | &b = %ld | b = %d\n", &b, &b, b);
+    printf("&c = %p | &c = %ld | c = %d\n", &c, &c, c);
+    printf("&d = %p | &d = %ld | d = %d\n", &d, &d, d);
+    printf("&e = %p | &d = %ld | e = %d\n\n", &e, &e, e);
+  
+
+    return 0;
+}
