8969f26835f4a5eccf845ec784e684842534332d
edersonschmeing <edersonschmeing@gmail.com>
2023-02-26 18:59:30 -0300

    endereço de variáveis

--- endereco-de-variaveis/endereco01.c
+++ endereco-de-variaveis/endereco01.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+
+
 int main() {
          
     int a = 30; 
-    printf("&a = %p \n", &a);
-  
+    printf("&a = %p \n\n", &a);
+
+    printf("O tamanho da váriavel ( a ) é %ld bytes\n\n", sizeof(a));
+    
+    unsigned long tamanho_a = sizeof(a);
+    printf("O tamanho da váriavel ( a ) é = %ld bytes\n\n", tamanho_a);   
+
     return 0;
 }
