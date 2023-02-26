2c92e1327edd3188d09015b54696584be06c1f30
edersonschmeing <edersonschmeing@gmail.com>
2023-02-26 16:34:51 -0300

    ponteiro_e_array

--- ponteiros-e-arrays/ponteiro_array.c
+++ ponteiros-e-arrays/ponteiro_array.c
@@ -4,23 +4,29 @@ int main() {
 
     int vec[5] = {5, 10, 15, 20, 25};
 
+    int *p = vec;
+
+
     //o nome de um vetor é um ponteiro para o primeiro índice. 
-    printf("vec = %p \n\n", vec);
+    printf("vec    = %p \n", vec);
+    printf("vec[0] = %p \n", &vec[0]);
+    printf("p      = %p \n\n", p);
 
+    printf("*vec   = %d \n", *vec);
+    printf("*p     = %d \n\n", *p);
 
-    printf("Índice\n");
+   
     for (int i = 0;i < 5;i++){
        printf("&vec[%d] = %p | vec[%d] = %d \n", i, &vec[i], i, vec[i]);
+       printf("p+%d     = %p | p+%d    = %d \n\n", i, p+i, i, *(p + i));
     }
 
     printf("\n"); 
     
-    int *p = vec;
-
-    printf("Aritmética de Ponteiros\n");    
+/*    printf("Aritmética de Ponteiros\n");    
     for (int i = 0;i < 5;i++){       
         printf("&p+%d  = %p | p+%d  = %d \n", i, p+i, i, *(p + i));
     }
-    
+  */  
     return 0;
 }
