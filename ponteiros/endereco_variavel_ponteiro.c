178ee43b0620f0bb295d52162d913d0f31025af7
edersonschmeing <edersonschmeing@gmail.com>
2023-02-26 20:59:10 -0300

    ponteiros

--- ponteiros/endereco_variavel_ponteiro.c
+++ ponteiros/endereco_variavel_ponteiro.c
@@ -2,7 +2,7 @@
 
 int main() {
     int a = 30;    
-    printf("&a = %p, %d \n", &a, a);
+    printf("&a = %p, a = %d \n", &a, a);
     
     int *p = &a;
     printf("&p = %p | p = %p | *p = %d\n\n", &p, p, *p);
