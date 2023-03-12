0e76c4ec5f798f07b2e90e9b3200a75f370f82c4
edersonschmeing <edersonschmeing@gmail.com>
2023-03-12 09:59:44 -0300

    funções recursivas

---
+++ funcoes-recursivas/fatorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+
+int fatorial(int valor){
+    if(valor <= 1 ){
+        return 1;
+    }
+    printf("%d * ", valor);
+    return valor * fatorial(valor - 1);
+}
+
+
+int main() {
+    
+    int valor;
+
+    printf("Informe um valor: ");
+    scanf("%d", &valor);
+    
+    int result = fatorial(valor);
+    printf("1 => ");
+    printf("%d! é : %d\n", valor, result);
+    return 0;
+}
