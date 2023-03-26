025ae664e1303fdb6189e52849169d6e9786c9ca
edersonschmeing <edersonschmeing@gmail.com>
2023-03-26 19:04:28 -0300

    deque como vetor

---
+++ deque-como-vetor/apps/app_main.c
@@ -0,0 +1,13 @@
+#include "deque_como_vetor.h"
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+int main() { 
+    
+   printf("\n--- Deque como Vetor ---\n\n");
+     
+   
+   return 0;
+
+}
---
+++ deque-como-vetor/src/deque_como_vetor.c
@@ -0,0 +1,5 @@
+#include "deque_como_vetor.h" 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>  
+
