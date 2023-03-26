498e49f73a4aff667742f32a2e7acdce71e7bb3d
edersonschmeing <edersonschmeing@gmail.com>
2023-03-26 19:01:32 -0300

    deque como lista ligada

---
+++ deque-como-lista-ligada/apps/app_main.c
@@ -0,0 +1,13 @@
+#include "deque_como_lista_ligada.h"
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+int main() { 
+    
+   printf("\n--- Deque como Lista Ligada ---\n\n");
+     
+   
+   return 0;
+
+}
