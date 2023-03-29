bcd59fb88387143edff7132ee73b4dddc4a00e4f
edersonschmeing <edersonschmeing@gmail.com>
2023-03-29 18:43:39 -0300

    lista ligada

--- lista-ligada/src/lista_ligada.c
+++ lista-ligada/src/lista_ligada.c
@@ -130,14 +130,14 @@ void adicionar_ordenado_lista(No **ptr_ptr_no, int valor) {
 
 } 
 
-int remover_inicio_lista(No **ptr_ptr_no) {
+bool remover_inicio_lista(No **ptr_ptr_no) {
  
      //TODO
-     return 1;
+     return true;
 
 } 
 
-int remover_fim_lista(No **ptr_ptr_no) {
+bool remover_fim_lista(No **ptr_ptr_no) {
 
     No *prt_atual = *ptr_ptr_no;
     No *ptr_no_anterior = NULL;
@@ -149,7 +149,7 @@ int remover_fim_lista(No **ptr_ptr_no) {
     ptr_no_anterior->proximo = NULL;
 
    //TODO
-   return 1;
+   return true;
 
 } 
 
