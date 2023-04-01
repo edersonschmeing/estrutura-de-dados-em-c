a0edb4fcfec556ba5ee91a539b01964029c79e46
edersonschmeing <edersonschmeing@gmail.com>
2023-04-01 13:46:05 -0300

    lista duplamente ligada

--- lista-duplamente-ligada/include/lista_duplamente_ligada.h
+++ lista-duplamente-ligada/include/lista_duplamente_ligada.h
@@ -8,3 +8,24 @@ typedef struct no No;
 typedef struct lista Lista;
 
 
+typedef struct no No;
+
+typedef struct lista Lista;
+
+Lista *criar_lista();
+void destruir_lista(Lista **lista);
+
+int tamanho_lista(Lista *lista);
+
+void imprimir_lista(Lista *lista);
+
+void adicionar_inicio_lista(Lista *lista, int valor);
+void adicionar_fim_lista(Lista *lista, int valor);
+void adicionar_posicao_lista(Lista *lista, int posicao);
+
+bool remover_inicio_lista(Lista *lista); 
+bool remover_fim_lista(Lista *lista); 
+bool remover_valor_lista(Lista *lista, int valor); 
+
+int buscar_valor_lista(Lista *lista, int valor);
+int buscar_posicao_lista(Lista *lista, int posicao);
--- lista-duplamente-ligada/src/lista_duplamente_ligada.c
+++ lista-duplamente-ligada/src/lista_duplamente_ligada.c
@@ -7,11 +7,91 @@ struct no {
     int dados; 
     No *proximo;  
     No *anterior;  
-
 };
 
 struct lista {
     int tamanho; 
     No *cabeca; 
-    No *cauda; 
+    No *cauda; // com ponteiro cauda/fim é um deque.   
 };
+
+
+Lista *criar_lista() {
+
+    //TODO: Implemente esta função.
+    return  NULL;
+
+}
+
+void destruir_lista(Lista **lista) { 
+
+    //TODO: Implemente esta função.
+
+}
+
+int tamanho_lista(Lista *lista) { 
+
+    //TODO: Implemente esta função.
+
+    return 0;
+}
+
+void imprimir_lista(Lista *lista) { 
+
+    //TODO: Implemente esta função.
+
+}
+
+void adicionar_inicio_lista(Lista *lista, int valor) { 
+
+    //TODO: Implemente esta função.
+
+}
+
+void adicionar_fim_lista(Lista *lista, int valor) { 
+
+    //TODO: Implemente esta função.
+
+}
+
+void adicionar_posicao_lista(Lista *lista, int posicao) { 
+
+    //TODO: Implemente esta função.
+
+}
+
+bool remover_inicio_lista(Lista *lista) { 
+
+    //TODO: Implemente esta função.
+    return true;
+
+} 
+
+
+bool remover_fim_lista(Lista *lista) {
+
+    //TODO: Implemente esta função.
+    return true;
+
+} 
+bool remover_valor_lista(Lista *lista, int valor) { 
+
+    //TODO: Implemente esta função.
+    return true;
+
+} 
+
+int buscar_valor_lista(Lista *lista, int valor) { 
+
+    //TODO: Implemente esta função.
+    return -1;
+
+}  
+
+int buscar_posicao_lista(Lista *lista, int posicao) { 
+
+    //TODO: Implemente esta função.
+    return -1;
+
+}
+
