f025d92ac99cc90f85a798aa2f1455699e3a6a7a
edersonschmeing <edersonschmeing@gmail.com>
2023-05-01 11:54:15 -0300

    Árvore Binária

--- arvore-binaria/app_main.c
+++ arvore-binaria/app_main.c
@@ -11,14 +11,15 @@ struct no {
     No *ptr_direita;
 };
 
+No *criar_no(int dado) { 
 
-No_Raiz *criar_AB() { 
-
-    No_Raiz *ptr_raiz = (No_Raiz*) malloc(sizeof(No_Raiz));
-    if (ptr_raiz != NULL) 
-        *ptr_raiz = NULL; 
+    No *ptr_no = (No*) malloc(sizeof(No*));
    
-    return ptr_raiz;
+    ptr_no->dado = dado;
+    ptr_no->ptr_direita = NULL;
+    ptr_no->ptr_esquerda = NULL;
+       
+    return ptr_no;
 
 }
 
@@ -33,6 +34,17 @@ void destruir_no_AB(No *ptr_no) {
     free(ptr_no);
     ptr_no = NULL;
         
+}
+
+No_Raiz *criar_AB() { 
+    
+    No_Raiz *ptr_raiz = (No_Raiz*) malloc(sizeof(No_Raiz));
+    
+    if (ptr_raiz != NULL) 
+        *ptr_raiz = NULL; 
+
+    return ptr_raiz;
+
 }
 
 void destruir_AB(No_Raiz *ptr_ptr_no_raiz) { 
@@ -52,6 +64,20 @@ int verificar_esta_vazia_AB(No_Raiz *ptr_no_raiz) {
 
 
 int verificar_altura_AB(No_Raiz *ptr_no_raiz) { 
+
+    if (ptr_no_raiz == NULL)
+        return 0;
+   
+    if (*ptr_no_raiz == NULL)
+        return 0;
+
+    int altura_sub_arvore_esquerda = verificar_altura_AB(&(*ptr_no_raiz)->ptr_esquerda);
+    int altura_sub_arvore_direita = verificar_altura_AB(&(*ptr_no_raiz)->ptr_direita);  
+
+    if (altura_sub_arvore_esquerda > altura_sub_arvore_direita) 
+        return altura_sub_arvore_esquerda + 1;     
+    else 
+        return altura_sub_arvore_direita + 1;
    
 }
 
@@ -61,113 +87,184 @@ int consultar_quantidade_no_AB(No_Raiz *ptr_no_raiz) {
   
 }
 
-
-void adicionar_AB(No_Raiz *ptr_no_raiz) {
-
-} 
-
-
-void remover_AB(No_Raiz *ptr_no_raiz) { 
-
-
-}
-
 //N - No
 //L - Esqueda
 //R - Direita
 
 //Pre-order (NLR) formatado
-void recursivo_percurso_pre_ordem_AB(No_Raiz root) { 
+void recursivo_percurso_pre_ordem_AB(No_Raiz ptr_no_raiz) { 
     
-    if (root == NULL)
-           return;
+    if (ptr_no_raiz == NULL)
+        return;
         
-    printf("%d -> ", root->dado);
-    recursivo_percurso_pre_ordem_AB(root->ptr_esquerda);
-    recursivo_percurso_pre_ordem_AB(root->ptr_direita);
+    printf("%d -> ", ptr_no_raiz->dado);
+    recursivo_percurso_pre_ordem_AB(ptr_no_raiz->ptr_esquerda);
+    recursivo_percurso_pre_ordem_AB(ptr_no_raiz->ptr_direita);
 
 }
 
+//Pre-order (NLR)
 void recursivo_percurso_pre_ordem_formatado_AB(No_Raiz ptr_no_raiz, int nivel) {
         
-        if (ptr_no_raiz == NULL)
-           return;
-        
-        for (int i = 0; i < nivel; i++)
-            printf(i == nivel - 1 ? "|-" : "  ");
-        
-        printf("%d\n", ptr_no_raiz->dado);
-        recursivo_percurso_pre_ordem_formatado_AB(ptr_no_raiz->ptr_esquerda, nivel + 1);
-        recursivo_percurso_pre_ordem_formatado_AB(ptr_no_raiz->ptr_direita, nivel + 1);
+    if (ptr_no_raiz == NULL)
+    return;
+    
+    for (int i = 0; i < nivel; i++)
+        printf(i == nivel - 1 ? "|-" : "  ");
+    
+    printf("%d\n", ptr_no_raiz->dado);
+    recursivo_percurso_pre_ordem_formatado_AB(ptr_no_raiz->ptr_esquerda, nivel + 1);
+    recursivo_percurso_pre_ordem_formatado_AB(ptr_no_raiz->ptr_direita, nivel + 1);
 }
 
 //In-order, (LNR)
 void recursivo_percurso_em_ordem_AB(No_Raiz ptr_no_raiz) { 
      
     if (ptr_no_raiz == NULL)
-           return;
+       return;
         
     recursivo_percurso_pre_ordem_AB(ptr_no_raiz->ptr_esquerda);
     printf("%d -> ", ptr_no_raiz->dado);
     recursivo_percurso_pre_ordem_AB(ptr_no_raiz->ptr_direita);
 
-
 }
 
 //Post-order, (LRN)
-void percurso_pos_ordem_AB(No_Raiz *ptr_no_raiz) {
+void recursivo_percurso_pos_ordem_AB(No_Raiz ptr_no_raiz) {
     
+    if (ptr_no_raiz == NULL)
+        return;
+
+    recursivo_percurso_pos_ordem_AB(ptr_no_raiz->ptr_esquerda);
+    recursivo_percurso_pos_ordem_AB(ptr_no_raiz->ptr_direita);
+    printf("%d -> ", ptr_no_raiz->dado);
+
 }
 
+void criar_arvore_heap(No_Raiz *ptr_ptr_no_raiz) {
+    
+    No *ptr_no0 = criar_no(6);  // raiz 
+    No *ptr_no1 = criar_no(0);  // filho a esquerda
+    No *ptr_no2 = criar_no(2);  // filho a direita
+    No *ptr_no3 = criar_no(9);  // filho a esquerda do 0
+    No *ptr_no4 = criar_no(1);  // filho a direita do 0
+    No *ptr_no5 = criar_no(4);  // filho a esquerda do 2
+    No *ptr_no6 = criar_no(7);  // filho a direita do 2
+    No *ptr_no7 = criar_no(8);  // filho a direita do 4
+    No *ptr_no8 = criar_no(10); // filho a direita do 1
+   
+    *ptr_ptr_no_raiz = ptr_no0;
+    
+    ptr_no0->ptr_esquerda = ptr_no1;
+    ptr_no0->ptr_direita = ptr_no2;
+
+    ptr_no1->ptr_esquerda = ptr_no3;
+    ptr_no1->ptr_direita = ptr_no4;
+
+    ptr_no2->ptr_esquerda = ptr_no5;
+    ptr_no2->ptr_direita = ptr_no6;
+     
+    ptr_no3->ptr_esquerda = ptr_no7;
+
+    ptr_no4->ptr_direita = ptr_no8;
+ 
+  
+    printf("Formato de Árvore \n");
+    recursivo_percurso_pre_ordem_formatado_AB(*ptr_ptr_no_raiz, 0);
+    
+    printf("\n\n");
+
+    printf("Pré-Ordem \n");
+    recursivo_percurso_pre_ordem_AB(*ptr_ptr_no_raiz);
+
+    printf("\n\n");
+
+    printf("Em-Ordem \n");
+    recursivo_percurso_em_ordem_AB(*ptr_ptr_no_raiz);
+
+    printf("\n\n");
+
+    printf("Pós-Ordem \n");
+    recursivo_percurso_pos_ordem_AB(*ptr_ptr_no_raiz);
+
+    printf("\n\n");
+
+    int altura_arvore_AB = verificar_altura_AB(ptr_ptr_no_raiz);
+    printf("Altrua da Árvore Binária: %d", altura_arvore_AB);
+
+    printf("\n\n");
+      
+}
+
+void criar_arvore_stack() {
+
+    No no0, no1, no2, no3, no4, no5, no6;
+
+    no0.dado = 6;
+    no0.ptr_esquerda = &no1;
+    no0.ptr_direita = &no2;
+
+    no1.dado = 2;
+    no1.ptr_esquerda = &no3;
+    no1.ptr_direita = &no4;
+
+    no2.dado = 9;
+    no2.ptr_esquerda = &no6;
+    no2.ptr_direita = NULL;
+
+    no3.dado = 1;
+    no3.ptr_esquerda = &no5;
+    no3.ptr_direita = NULL;
+
+    no4.dado = 4;
+    no4.ptr_esquerda = NULL;
+    no4.ptr_direita = NULL;
+
+    no5.dado = 8;
+    no5.ptr_esquerda = NULL;
+    no5.ptr_direita = NULL;
+
+    no6.dado = 7;
+    no6.ptr_esquerda = NULL;
+    no6.ptr_direita = NULL;
+
+    printf("Formato de Árvore \n");
+    recursivo_percurso_pre_ordem_formatado_AB(&no0, 0);
+    
+    printf("\n");
+
+    printf("Pré-Ordem \n");
+    recursivo_percurso_pre_ordem_AB(&no0);
+
+    printf("\n\n");
+
+    printf("Em-Ordem \n");
+    recursivo_percurso_em_ordem_AB(&no0);
+
+    printf("\n\n");
+
+    printf("Pós-Ordem \n");
+    recursivo_percurso_pos_ordem_AB(&no0);
+
+    printf("\n\n");
+
+
+}
 
 int main() { 
     
-   printf("\n--- Árvore Binária ---\n\n");
+   printf("\n--- Árvore Binária com alocação na Heap ---\n\n");
 
-   //No_Raiz *ptr_ptr_no_raiz = criar_AB();  
+   No_Raiz *ptr_ptr_no_raiz = criar_AB();  
 
-   //destruir_ABB(ptr_ptr_no_raiz);
-   
-        No a, b, c, d, e, f, g;
+   criar_arvore_heap(ptr_ptr_no_raiz);
 
-        a.dado = 6;
-        a.ptr_esquerda = &b;
-        a.ptr_direita = &c;
+   destruir_AB(ptr_ptr_no_raiz);
 
-        b.dado = 2;
-        b.ptr_esquerda = &d;
-        b.ptr_direita = &e;
 
-        c.dado = 9;
-        c.ptr_esquerda = &g;
-        c.ptr_direita = NULL;
+   printf("\n\n--- Árvore Binária com alocação na Stack ---\n\n");
 
-        d.dado = 1;
-        d.ptr_esquerda = &f;
-        d.ptr_direita = NULL;
-
-        e.dado = 4;
-        e.ptr_esquerda = NULL;
-        e.ptr_direita = NULL;
-
-        f.dado = 8;
-        f.ptr_esquerda = NULL;
-        f.ptr_direita = NULL;
-
-        g.dado = 8;
-        g.ptr_esquerda = NULL;
-        g.ptr_direita = NULL;
-
-        recursivo_percurso_pre_ordem_formatado_AB(&a, 0);
-        
-        printf("\n");
-
-        recursivo_percurso_pre_ordem_AB(&a);
-
-        printf("\n\n");
-
-        recursivo_percurso_em_ordem_AB(&a);
-
+   criar_arvore_stack();
 
    return 0;
 
